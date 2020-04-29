874ad48518169cc8b0a21a155e133c2fa96220ed
Jeff Tian <Jeff.Tian@externals.adidas.com>
2020-04-29 10:27:31 +0800

    fix: add babel-helper

--- examples/gatsbyjs/src/pages/index.js
+++ examples/gatsbyjs/src/pages/index.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 import { withKeycloak } from "@react-keycloak/web"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = withKeycloak(({ keycloak }) => (
@@ -12,9 +11,7 @@ const IndexPage = withKeycloak(({ keycloak }) => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
     <Link to="/page-2/">Go to page 2</Link>
     <div>
       <div>{`User is ${
